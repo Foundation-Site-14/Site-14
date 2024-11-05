ddb0a175e4a394f45a6bfebadfb7f6ae45f7f885
August Eymann <august.eymann@gmail.com>
2024-11-04 18:25:01 -0800

    feat: added mp5, mtf gear, and nvg

---
+++ Content.Server/_SCP/NightVision/NightVisionSystem.cs
@@ -0,0 +1,5 @@
+﻿using Content.Shared._SCP.NightVision;
+
+namespace Content.Server._SCP.NightVision;
+
+public sealed class NightVisionSystem : SharedNightVisionSystem;
---
+++ Content.Shared/_SCP/NightVision/GreenVisionComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._SCP.NightVision;
+
+[RegisterComponent]
+[NetworkedComponent]
+
+public sealed partial class GreenVisionComponent : Component
+{}
